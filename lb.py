305f616e0382e03a215284f07fa74e7ee6b735d0
YoussefSaad3000 <youssefsaad3000@gmail.com>
2024-08-25 19:43:18 +0100

    Added Concurrent request tst file

--- lb.py
+++ lb.py
@@ -5,15 +5,11 @@ import requests
 
 app = Flask(__name__)
 lb = None
-# event = threading.Event()
+
 
 @app.route('/')
 def home():
     global lb
-    print("hna f home:", lb)
-    print("event 9bal wait : ",event)
-    # event.wait()
-    print("event after wait: ",event)
     server = lb.get_next_server()
     if not server:
         return "No available server at this time "
@@ -27,30 +23,12 @@ def home():
     except Exception as e:
         return f"Exception occurred while fetching the server {e}"
 
-# def run_load_balancer():
-#     global lb
-#     print("run_load_balancer lb 1 ", lb)
-#     # servers = Server.get_servers_from_endpoints(["http://localhost:8080", "http://localhost:8081", "http://localhost:8082"])
-#     # lb = LoadBalancer(servers)
-#     lb.health_check_ticker()
-#     print("run_load_balancer lb 2 ", lb)
-#     # # event.set()
-#     # print("event: ",event)
-
 
 
 if __name__ == '__main__':
-    # Use host='0.0.0.0' to make the server publicly accessible
+   
     servers = Server.get_servers_from_endpoints(["http://localhost:8080", "http://localhost:8081", "http://localhost:8082"])
     lb = LoadBalancer(servers)
-    print("lb in main 1")
-    # lb_thread = threading.Thread(target=run_load_balancer)
-
-    # lb_thread.daemon = True  
-    # lb_thread.start()
-
-    
-    print("lb in main 2  ", lb)
     
     app.run(host='0.0.0.0', port=80)
 
